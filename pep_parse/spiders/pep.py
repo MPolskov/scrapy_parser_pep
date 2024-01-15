1eea453def0927d324940cfd1e218f15199179fd
MPolsh <m@polshkov.ru>
2024-01-15 09:20:27 +0500

    pep spider. In progress. rev.0.1

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,5 +1,9 @@
 import scrapy
 
+from urllib.parse import urljoin
+
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -7,4 +11,19 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        section_tag = response.css('section#numerical-index')
+        table_body = section_tag.css('tbody')
+        for row in table_body.css('tr'):
+            pep_link = urljoin(
+                self.start_urls, row.css('td:nth-child(3) a::attr(href)')
+            )
+            status = response.follow(pep_link, callback=self.parse_status)
+            date = {
+                'name': row.css('td:nth-child(3) a::text').get(),
+                'number': row.css('td:nth-child(2) a::text').get(),
+                'status': status
+            }
+            yield PepParseItem(date)
+
+    def parse_status(self, response):
+        return response.css('dl.rfc2822 dd:nth-child(4) abbr::text').get()
